4792425d5ac35ab1f167ac7da90450e7ae33b154
CaioMorenoGoulart <caiogoulartprogramacao@gmail.com>
2025-07-09 22:54:20 -0300

    commit  -m ""

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const apiKeyInput = document.getElementById('apiKey');
 const gameSelect = document.getElementById('gameSelect');
 const questionInput = document.getElementById('questionInput');
 const askButton = document.getElementById('askButton');
+const aiResponse = document.getElementById('aiResponse');
 const form = document.getElementById('form');
 
 const enviarFormulario = (event) => {
